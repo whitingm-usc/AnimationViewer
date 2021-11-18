4a4c82e31442c25cd05748cfa981c219863f37cc
Matt Whiting <whitingm@usc.edu>
2021-11-17 18:02:24 -0800

    Added Assassin character with more detailed controller script

---
+++ Assets/Scripts/Assassin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assassin : TestAnimation
+{
+    // Update is called once per frame
+    protected override void Update()
+    {
+        {   // Handle Die
+            bool doDie = false;
+            if (Input.GetKeyDown(KeyCode.X))
+                doDie = true;
+            if (doDie)
+                m_Animator.SetTrigger("Die");
+        }
+
+        {   // Handle Special
+            bool doSpecial = false;
+            if (Input.GetKeyDown(KeyCode.Tab))
+                doSpecial = true;
+            if (doSpecial)
+                m_Animator.SetTrigger("Special");
+        }
+
+        {   // Handle Wound
+            bool doSpecial = false;
+            if (Input.GetKeyDown(KeyCode.Z))
+                doSpecial = true;
+            if (doSpecial)
+                m_Animator.SetTrigger("Wound");
+        }
+
+        {
+            bool swordToggle = false;
+            if (Input.GetKeyDown(KeyCode.Backspace))
+                swordToggle = true;
+            if (swordToggle)
+            {
+                bool swordOut = m_Animator.GetBool("IsSwordOut");
+                m_Animator.SetBool("IsSwordOut", !swordOut);
+            }
+        }
+
+        base.Update();
+    }
+}
--- Assets/Scripts/TestAnimation.cs
+++ Assets/Scripts/TestAnimation.cs
@@ -6,23 +6,33 @@ public class TestAnimation : MonoBehaviour
 {
     public float turnSpeed = 20f;
 
-    Animator m_Animator;
+    protected Animator m_Animator;
 
     void Start()
     {
         m_Animator = GetComponent<Animator>();
     }
 
-    void Update()
+    protected virtual void Update()
     {
-        bool doJump = false;
-        if (Input.GetKeyDown(KeyCode.Space))
-            doJump = true;
-        if (doJump)
-            m_Animator.SetTrigger("DoJump");
+        {
+            bool doJump = false;
+            if (Input.GetKeyDown(KeyCode.Space))
+                doJump = true;
+            if (doJump)
+                m_Animator.SetTrigger("DoJump");
+        }
+
+        {
+            bool doAttack = false;
+            if (Input.GetKeyDown(KeyCode.Return))
+                doAttack = true;
+            if (doAttack)
+                m_Animator.SetTrigger("Attack");
+        }
     }
 
-    void FixedUpdate()
+    protected virtual void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
